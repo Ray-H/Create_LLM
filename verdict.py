0756d41e690051ac3e3dd9539e067c1fdc2b03e6
Ray Higashi <rayhigashi@Rays-MacBook-Pro.local>
2026-05-20 19:57:58 -0600

    added data loader

--- verdict.py
+++ verdict.py
@@ -1,9 +1,10 @@
 from __future__ import annotations
 
 from collections.abc import Iterable
+from pathlib import Path
 
 import tiktoken
-
+from gptDataset import GPTDatasetV1
 
 class TikTokenizer:
     def __init__(
@@ -34,20 +35,35 @@ class TikTokenizer:
 
 
 if __name__ == "__main__":
-    tokenizer = TikTokenizer(allowed_special={"<|endoftext|>"})
-    text = "Akwirw ier"
-    token_ids = tokenizer.encode(text)
-    with open("/Users/rayhigashi/development/LLMs-from-scratch-main/ch02/my_code/the-verdict.txt", "r", encoding="utf-8") as f:
+    text_path = Path(__file__).parent / "the-verdict.txt"
+
+    with open(text_path, "r", encoding="utf-8") as f:
         raw_text = f.read()
 
-    enc_text = tokenizer.encode(raw_text)
-    print(len(enc_text))
-    enc_sample = enc_text[50:]
-    context_size = 4
-    x = enc_sample[:context_size]
-    y = enc_sample[1:context_size+1]
-    print(f"x: {x}")
-    print(f"y:      {y}")
+    dataloader = GPTDatasetV1.create_dataloader_v1(
+        raw_text, batch_size=1, max_length=4, stride=1, shuffle=False)
+    data_iter = iter(dataloader)
+    first_batch = next(data_iter)
+    print(first_batch)
+
+    # tokenizer = TikTokenizer(allowed_special={"<|endoftext|>"})
+    # text = "Akwirw ier"
+    # token_ids = tokenizer.encode(text)
+    # with open("/Users/rayhigashi/development/LLMs-from-scratch-main/ch02/my_code/the-verdict.txt", "r", encoding="utf-8") as f:
+    #     raw_text = f.read()
+
+    # enc_text = tokenizer.encode(raw_text)
+    # print(len(enc_text))
+    # enc_sample = enc_text[50:]
+    # context_size = 4
+    # for i in range(1, context_size+1):
+    #     context = enc_sample[:i]
+    #     desired = enc_sample[i]
+    #     print(tokenizer.decode(context), "---->", tokenizer.decode([desired]))
+    # x = enc_sample[:context_size]
+    # y = enc_sample[1:context_size+1]
+    # print(f"x: {x}")
+    # print(f"y:      {y}")
     # print("Text:", text)
     # print("Token IDs:", token_ids)
     # print("Token count:", tokenizer.count_tokens(text))
