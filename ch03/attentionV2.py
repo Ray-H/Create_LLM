78358d36b015811cdab4d0bb2dd537cc22de7ec6
Ray Higashi <rayhigashi@Rays-MacBook-Pro.local>
2026-05-30 16:09:34 -0600

    finished 3.4

---
+++ ch03/attentionV2.py
@@ -0,0 +1,40 @@
+import torch
+
+
+class SelfAttention_v2(torch.nn.Module):
+    def __init__(self, d_in, d_out):
+        super().__init__()
+        self.W_query = torch.nn.Linear(d_in, d_out, bias=False)
+        self.W_key = torch.nn.Linear(d_in, d_out, bias=False)
+        self.W_value = torch.nn.Linear(d_in, d_out, bias=False)
+        
+
+    def forward(self, x):
+   
+        queries = self.W_query(inputs)
+        keys = self.W_key(inputs)
+        values = self.W_value(inputs)
+
+        d_k = keys.shape[1]        
+        attn_scores = queries @ keys.T
+        attn_weights = torch.softmax(attn_scores/d_k**0.5, dim=-1)
+        context_vec = attn_weights @ values
+     
+        return context_vec
+
+torch.manual_seed(123)
+inputs = torch.tensor(
+  [[0.43, 0.15, 0.89], # Your     (x^1)
+   [0.55, 0.87, 0.66], # journey  (x^2)
+   [0.57, 0.85, 0.64], # starts   (x^3)
+   [0.22, 0.58, 0.33], # with     (x^4)
+   [0.77, 0.25, 0.10], # one      (x^5)
+   [0.05, 0.80, 0.55]] # step     (x^6)
+)
+d_in = inputs.shape[1]
+d_out=2
+sa_v2 = SelfAttention_v2(d_in,d_out)
+print(sa_v2(inputs))
+
+
+
